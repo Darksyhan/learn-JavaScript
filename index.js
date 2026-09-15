0d66f979035ce18f560282bd7692bf3b541f3f51
Darksyhan <hansy1310@naver.com>
2026-09-15 13:52:07 +0900

    함수 에 대한 코딩

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-function init() {
-  alert("교육");
-}
-
 console.log("Hello Js");
 
 // 변수 - 값을 저장하는 이름을 가진 공간
@@ -44,8 +40,64 @@ const isDarkMode = true; // boolean
 console.log("🚀 ~ isDarkMode:", isDarkMode);
 console.log("🚀 ~ isDarkMode:", typeof isDarkMode);
 
+// 변수 선언 및 null 셋팅
 const empty = null;
 console.log("🚀 ~ empty:", empty, "/ num empty", typeof empty);
 
-let userage;
+let userage; // 변수 선언
 console.log("🚀 ~ userage:", userage);
+
+/***************************************************************************
+ * 2교실 - 함수
+ * 특정 기능을 수행하도록 만든 코드 조각
+ ***************************************************************************/
+function init() {
+  // 함수 실행한다, 호출한다.
+  //fngreeting();
+  greeting("한상열", "극-J"); // "극-J"라는 인자값을 보냈다.
+
+  fnView();
+  arrowView();
+
+  let sum = add(10, 20);
+  console.log("🚀 ~ init ~ sum:", sum);
+  console.log("🚀 ~ init ~ doubleSum:", doubleSum(5));
+}
+
+// 함수를 정의한다, 선언한다. 라고 표현
+function fngreeting() {
+  //실행 로직
+  //console.log("???님, 반갑습니다.");
+  console.log(`${userNm}님, 반갑습니다.`); // 백틱 문법 ??
+
+  console.log("안녕하세요 " + userNm + "님");
+  console.log(userNm + "님, 반갑습니다.");
+}
+
+// 화살표 함수 변환
+const greeting = (studentname, psMbti) => {
+  // psMbti : 파라미터로서 값을 받아주는 변수
+  //실행 로직
+  console.log(`${studentname}님, 반갑습니다.`);
+  console.log(`MBTI는 ${psMbti}이군요`);
+};
+
+// 미션 - 숙제 : "프론트엔드부터 만드는 AI챗봇 수강생 ???입니다."
+function fnView() {
+  console.log("프론트엔드부터 만드는 AI챗봇 수강생 " + userNm + "입니다.");
+}
+
+const arrowView = () => {
+  // console.log("프론트엔드부터 만드는 AI챗봇 수강생 " + userNm + "입니다.");
+  console.log(`프론트엔드부터 만드는 AI챗봇 수강생 ${userNm}입니다.`);
+};
+
+const doubleSum = (num) => {
+  console.log("🚀 ~ doubleSum ~ doubleSum:", doubleSum);
+  return num * 2;
+};
+
+// 결과 값 반환 하는 함수
+const add = (num1, num2) => {
+  return num1 + num2;
+};
