f18c6dca01d8864dadb8bb0cfa789c10e8a761a1
Darksyhan <hansy1310@naver.com>
2026-09-15 10:13:57 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function init() {
+  alert("교육");
+}
+
+console.log("Hello Js");
+
+// 변수 - 값을 저장하는 이름을 가진 공간
+/** const 변경 불가 변수 , let 변경 가능한 변수 (var과 동일)
+ */
+const userNm = "다크시니";
+let userTit = "java Script 교육";
+
+console.log("userNm", userNm);
+
+// const의 변조를 시도 하였으나 스크립트 오류가 발생
+// userNm = "시니냥";
+// console.log(userNm);
